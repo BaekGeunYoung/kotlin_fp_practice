2ce3a95e6fb2afd30ce91a5d11a9f12ab1f213d4
BaekGeunYoung <dvmflstm@gmail.com>
2020-11-02 00:40:39 +0900

    studying chapter 7

--- src/main/kotlin/paralellism/Par.kt
+++ src/main/kotlin/paralellism/Par.kt
@@ -8,16 +8,43 @@ import java.util.concurrent.TimeUnit
 typealias Par<A> = (ExecutorService) -> Future<A>
 
 fun <A> unit(a: A): Par<A> = { _ -> UnitFuture(a) }
+
 fun <A> fork(a: () -> Par<A>): Par<A> = { es ->
     es.submit(Callable { a()(es).get() })
 }
 
 fun <A> lazyUnit(a: () -> A): Par<A> = fork { unit(a()) }
+
 fun <A> run(s: ExecutorService, a: Par<A>): Future<A> = a(s)
+
 fun <A, B, C> map2(par1: Par<A>, par2: Par<B>, f: (A, B) -> C): Par<C> = { es ->
     val af = par1(es)
     val bf = par2(es)
     UnitFuture(f(af.get(), bf.get()))
+}
+
+fun <A, B> map(pa: Par<A>, f: (A) -> B): Par<B> = map2(pa, unit(Unit)) { a, _ -> f(a) }
+
+fun sortPar(parList: Par<List<Int>>): Par<List<Int>> = map(parList) { list -> list.sorted() }
+
+fun <A, B> parMap(ps: List<A>, f: (A) -> B): Par<List<B>> = fork {
+    val fbs: List<Par<B>> = ps.map { asyncF(f)(it) }
+    sequence(fbs)
+}
+
+fun <A> parFilter(aSeq: List<A>, f: (A) -> Boolean): Par<List<A>> = fork {
+    val pars: List<Par<List<A>>> = aSeq.map(
+            asyncF { a: A -> if (f(a)) listOf(a) else listOf() }
+    )
+
+    map(sequence(pars)) {
+        it.flatten()
+    }
+}
+
+fun <A> sequence(ps: List<Par<A>>): Par<List<A>> = { es ->
+    val list: List<A> = ps.map { it(es).get() }
+    UnitFuture(list)
 }
 
 class UnitFuture<A>(
@@ -51,3 +78,9 @@ fun <A, B> asyncF(f: (A) -> B): (A) -> Par<B> =
         { a ->
             lazyUnit { f(a) }
         }
+
+fun test() {
+    val f: (Int) -> String = { it.toString() }
+
+
+}
