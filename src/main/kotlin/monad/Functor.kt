290034cbf57c9f1029b9693fa4bb25b85ebba47e
BaekGeunYoung <dvmflstm@gmail.com>
2021-01-03 18:58:24 +0900

    studying functor

---
+++ src/main/kotlin/monad/Functor.kt
@@ -0,0 +1,27 @@
+package monad
+
+import arrow.Kind
+import arrow.core.Either
+import arrow.higherkind
+
+interface Functor <F> {
+    fun <A, B> Kind<F, A>.map(f: (A) -> B): Kind<F, B>
+    fun <A, B> Kind<F, Pair<A, B>>.distribute(): Pair<Kind<F, A>, Kind<F, B>> = this.map { it.first } to this.map { it.second }
+    fun <A, B> Either<Kind<F, A>, Kind<F, B>>.codistribute(): Kind<F, Either<A, B>> =
+        if (this.isLeft()) (this as Either.Left<Kind<F, A>>).a.map { Either.Left(it) }
+        else (this as Either.Right<Kind<F, B>>).b.map { Either.Right(it) }
+}
+
+class ForListK private constructor() { companion object }
+
+@Suppress("UNCHECKED_CAST")
+fun <A> Kind<ForListK, A>.fix(): ListK<A> = this as ListK<A>
+
+@higherkind
+data class ListK<T>(val list: List<T>) : Kind<ForListK, T>, List<T> by list {
+    fun <B> map(f: (T) -> B): ListK<B> = ListK(this.list.map(f))
+}
+
+class ListFunctor : Functor<ForListK> {
+    override fun <A, B> Kind<ForListK, A>.map(f: (A) -> B): Kind<ForListK, B> = fix().map(f)
+}
--- src/main/kotlin/monoid/Foldable.kt
+++ src/main/kotlin/monoid/Foldable.kt
@@ -1,14 +1,25 @@
 package monoid
 
 import arrow.Kind
-import monoid.FoldableList.foldRight
+import arrow.core.extensions.list.foldable.foldLeft
 
 interface Foldable<F> {
     fun <A, B> Kind<F, A>.foldRight(z: B, f: (A, B) -> B): B
     fun <A, B> Kind<F, A>.foldLeft(z: B, f: (A, B) -> B): B
     fun <A, B> Kind<F, A>.foldMap(f: (A) -> B, mb: Monoid<B>): B
+    fun <A> Kind<F, A>.toList(): List<A> = this.foldRight(
+        listOf(),
+        { a, b -> b.toMutableList().apply { add(a) } }
+    )
     fun <A> Kind<F, A>.concatenate(m: Monoid<A>) = this.foldLeft(m.zero) { x, y -> m.op(x, y) }
 }
+
+class ListConcatMonoid<T> : Monoid<List<T>> {
+    override val zero: List<T> = listOf()
+    override fun op(a1: List<T>, a2: List<T>): List<T> =
+        a1.toMutableList().apply { addAll(a2) }
+}
+
 
 class ForList private constructor()
 typealias ListOf<A> = Kind<ForList, A>
@@ -56,13 +67,105 @@ object FoldableList : Foldable<ForList> {
 
         return acc
     }
+
+    override fun <A> Kind<ForList, A>.toList(): List<A> {
+        TODO("Not yet implemented")
+    }
 }
 
 fun main() {
-    val fList: ListOf<Int> = FoldableList.of(1, 2, 3)
-    val test = fList.foldRight("") { x, y ->
-        x.toString() + x.toString() + x.toString() + y
+    val bagA = bagUsingMonoid(listOf("a", "b", "c", "d", "a"))
+    println(bagA)
+}
+
+sealed class Tree<A>
+
+class Leaf<A>(val value: A) : Tree<A>()
+class Branch<A>(val left: Tree<A>, val right: Tree<A>) : Tree<A>()
+
+object FoldableTree {
+    fun <A, B> Tree<A>.foldRight(z: B, f: (A, B) -> B): B =
+        when (this) {
+            is Leaf -> f(this.value, z)
+            is Branch -> this.left.foldRight(this.right.foldRight(z, f), f)
+        }
+
+    fun <A, B> Tree<A>.foldLeft(z: B, f: (A, B) -> B): B =
+        when (this) {
+            is Leaf -> f(this.value, z)
+            is Branch -> this.right.foldLeft(this.left.foldLeft(z, f), f)
+        }
+
+    fun <A, B> Tree<A>.foldMap(f: (A) -> B, mb: Monoid<B>): B =
+        when (this) {
+            is Leaf -> f(this.value)
+            is Branch -> mb.op(this.left.foldMap(f, mb), this.right.foldMap(f, mb))
+        }
+}
+
+sealed class Option<out A>
+
+class Some<out A>(val value: A) : Option<A>()
+object None: Option<Nothing>()
+
+object FoldableOption {
+    fun <A, B> Option<A>.foldRight(z: B, f: (A, B) -> B): B =
+        when (this) {
+            is None -> z
+            is Some -> f(this.value, z)
+        }
+
+    fun <A, B> Option<A>.foldLeft(z: B, f: (A, B) -> B): B =
+        when (this) {
+            is None -> z
+            is Some -> f(this.value, z)
+        }
+
+    fun <A, B> Option<A>.foldMap(f: (A) -> B, mb: Monoid<B>): B =
+        when (this) {
+            is None -> mb.zero
+            is Some -> f(this.value)
+        }
+}
+
+fun <A, B> productMonoid(a: Monoid<A>, b: Monoid<B>): Monoid<Pair<A, B>> {
+    return object : Monoid<Pair<A, B>> {
+        override val zero: Pair<A, B> = a.zero to b.zero
+        override fun op(a1: Pair<A, B>, a2: Pair<A, B>): Pair<A, B> =
+            a.op(a1.first, a2.first) to b.op(a1.second, a2.second)
+    }
+}
+
+fun <A, B> functionMonoid(b: Monoid<B>): Monoid<(A) -> B> {
+    return object : Monoid<(A) -> B> {
+        override val zero: (A) -> B = { b.zero }
+        override fun op(a1: (A) -> B, a2: (A) -> B): (A) -> B = {
+            b.op(a1(it), a2(it))
+        }
+    }
+}
+
+fun <A> bag(aSeq: List<A>): Map<A, Int> =
+    aSeq.foldRight(mutableMapOf()) { a, b ->
+        b.apply {
+            if (b.containsKey(a)) {
+                b[a] = b[a]!! + 1
+            }
+            else {
+                b[a] = 1
+            }
+        }
     }
 
-    println(test)
-}
+fun <K, V> mapMergeMonoid(v: Monoid<V>): Monoid<Map<K, V>> =
+    object : Monoid<Map<K, V>> {
+        override val zero: Map<K, V> = mapOf()
+        override fun op(a1: Map<K, V>, a2: Map<K, V>): Map<K, V> =
+            (a1.keys + a2.keys).fold(zero) { acc, k ->
+                acc.toMutableMap().apply {
+                    put(k, v.op(a1.getOrDefault(k, v.zero), a2.getOrDefault(k, v.zero)))
+                }
+            }
+    }
+
+fun <A> bagUsingMonoid(aSeq: List<A>): Map<A, Int> = foldMap(aSeq, mapMergeMonoid<A, Int>(intAddition)) { mapOf(it to 1) }
